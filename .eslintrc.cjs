39bf6ed2361a7d904e0e298da384cd9e1125c8a3
taezzang <eaqwrd@gmail.com>
2023-01-18 18:00:36 +0900

    기본 폴더 구조 생성

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,13 @@ module.exports = {
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-undef': 'off',
         'no-unused-vars': 'off',
+        'vue/multi-word-component-names': [
+            'error',
+            {
+                // Vue 컴포넌트의 이름은 여러 개의 단어를 섞어서 지어야 하지만 ignores에 적힌 이름들은 무시함.
+                ignores: ['index'],
+            },
+        ],
         'prettier/prettier': [
             'error',
             {
---
+++ components/TestComp.vue
@@ -0,0 +1,3 @@
+<template>
+    <div><h2>테스트 컴포넌트</h2></div>
+</template>
