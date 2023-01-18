4f589620603a40d595f2b05c97c46e86ed00649d
taezzang <eaqwrd@gmail.com>
2023-01-18 14:14:29 +0900

    meta 태그 설정 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+    app: {
+        head: {
+            charset: 'utf-8',
+            title: 'Nuxt BoilerPlate',
+            meta: [
+                // <meta name="description" content="My amazing site">
+                { name: 'description', content: 'This is Nuxt3 BolierPlate' },
+            ],
+        },
+    },
+});
