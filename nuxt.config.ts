c15470aa0f3c28edb140076b6d64ee39ef92022b
taezzang <eaqwrd@gmail.com>
2023-01-22 16:32:34 +0900

    sitemap 모듈 추가 후 설정 완료

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,28 @@
 import messages from './i18n/messages';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/i18n', '@pinia/nuxt', ['@nuxtjs/robots', { configPath: './robots.config' }]],
+    modules: [
+        '@nuxtjs/i18n',
+        '@pinia/nuxt',
+        ['@nuxtjs/robots', { configPath: './robots.config' }],
+        'nuxt-simple-sitemap',
+    ],
+    // https://nuxt.com/modules/simple-sitemap
+    sitemap: {
+        // sitemap.xml 생성 시 사이트 호스트를 제공해야함
+        hostname: 'http://localhost:3000', // 임시 설정, 실 배포 시엔 변경 필요
+    },
+    // 다이내믹 루트 처리
+    // 기본적으로 모든 스태틱 루트는 sitemap.xml에 포함됨
+    // 다이내믹 루트를 포함할 있도록 설정 추가
+    nitro: {
+        // 자동 동적 URL 설정 <- Simple-sitemap 모듈 권장 설정
+        // 사이트에 동적 링크가 연결되어 있는 경우 Nitro Crawler가 자동으로 링크를 포함하도록 설정
+        prerender: {
+            crawlLinks: true,
+            routes: ['/'],
+        },
+    },
     i18n: {
         // strategy: 'no_prefix', // 해당 옵션 활성화 시 root 도메인 바로 앞에 /ko 또는, /en 형식으로 locale이 따라 붙지 않음
         detectBrowserLanguage: {
