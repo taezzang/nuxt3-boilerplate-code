4a1d6d4653589964acb0b4c7e297f2ab0ddb2211
taezzang <eaqwrd@gmail.com>
2023-01-29 17:27:17 +0900

    ts(2349) 에러 임시 해결

--- pages/i18n.vue
+++ pages/i18n.vue
@@ -2,12 +2,12 @@
     <div class="section">
         <h2>i18n 테스트 컴포넌트</h2>
         <p>i18n 테스트 : {{ t('welcome') }}</p>
-        <NuxtLink v-for="locale in availableLocales" :key="locale.code" :to="switchLocalePath(locale.code)">{{
-            locale.name
+        <NuxtLink v-for="lang in availableLocales" :key="lang.code" :to="switchLocalePath(lang.code)">{{
+            lang.name
         }}</NuxtLink>
     </div>
 </template>
-<script setup lang="ts">
+<script setup>
 const { t, locale, locales } = useI18n();
 const switchLocalePath = useSwitchLocalePath();
 const availableLocales = computed(() => locales.value.filter((i) => i.code !== locale.value));
