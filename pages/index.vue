232d245d271fc2b8f33ed089441737325a21c62f
taezzang <eaqwrd@gmail.com>
2023-02-09 18:18:36 +0900

    백그라운드 이미지 nuxt3 의 에셋 내 이미지 불러오기 테스트

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
 <template>
     <div class="section">
         <div class="section__wrap">
-            <img src="/images/test.png" alt="" />
+            <img src="~/assets/images/logo.png" alt="" />
+            <div class="backImg"></div>
             <h1>Home Page</h1>
         </div>
     </div>
 </template>
+<script setup lang="ts"></script>
+<style scoped>
+.backImg {
+    width: 200px;
+    height: 200px;
+    background: url('~/assets/images/logo.png') no-repeat;
+    background-size: 100%;
+}
+</style>
