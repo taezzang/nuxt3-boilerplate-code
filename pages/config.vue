81b0dafb67408c0b5f4b1c00fc3348e008ff9f2b
taezzang <eaqwrd@gmail.com>
2023-02-16 17:51:24 +0900

    vue 인스턴스에서 runtimeConfig 확인 코드  추가

---
+++ pages/config.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="section">
+        <div>Check developer console!</div>
+    </div>
+</template>
+<script setup lang="ts">
+const config = useRuntimeConfig();
+console.log('Runtime config:', config.public);
+
+console.log('API Base URL:', config.apiBase);
+</script>
