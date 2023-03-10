fd16d8259de1a2bc7cebf6d0a50ddf4cff1a3d24
taezzang <eaqwrd@gmail.com>
2023-03-10 19:16:13 +0900

    router 하위 페이지 추가

---
+++ pages/children/[type].vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="section">
+        <h3>pages 폴더 내부에 하위 폴더 생성 시 router의 children과 동일한 기능을 한다</h3>
+        <h3>현재 children이라는 하위 폴더 내부에 [type].vue 파일이 생성되어 있으며 type은 파라미터로 전송된다.</h3>
+        <h1>현재 children : {{ $route.params.type }}</h1>
+    </div>
+</template>
